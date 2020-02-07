d11428547558cfaf3d21f487fb2352bade907595
wilson olabode <wilson olabode@WILSON>
2020-02-07 13:51:17 +0100

    pages

---
+++ Admin/Pages/Users/Index.cshtml.cs
@@ -0,0 +1,70 @@
+﻿using Admin.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Admin
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        [BindProperty]
+        public Member Member { get; set; }
+
+        public IndexModel(ApplicationDbContext context, UserManager<IdentityUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+        public async Task OnGet()
+        {
+            var user = (await GetCurrentUserAsync()).Id;
+
+            Member = await _context.Members.FirstOrDefaultAsync(x => x.Id == user);
+        }
+
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            var member = await _context.Members.FirstOrDefaultAsync(x => x.Id == Member.Id);
+
+
+            member.Firstname = Member.Firstname;
+            member.Surname = Member.Surname;
+            member.Middlename = Member.Middlename;
+            member.PhoneNumber = Member.PhoneNumber;
+            member.WhatsAppNumber = Member.WhatsAppNumber;
+            member.SponsorName = Member.SponsorName;
+            member.DateOfBirth = Member.DateOfBirth;
+            member.NameOfUpLine = Member.NameOfUpLine;
+            member.Country = Member.Country;
+            member.State = Member.State;
+            member.Title = Member.Title;
+            member.Address = Member.Address;
+            member.City = Member.City;
+            member.RegistrationDate = Member.RegistrationDate;
+
+
+           
+            
+            
+            await _context.SaveChangesAsync();
+
+            return Page();
+       
+        }
+
+
+    private Task<IdentityUser> GetCurrentUserAsync() => _userManager.GetUserAsync(HttpContext.User);
+    }
+}
---
+++ Admin/Pages/Users/PaymentDetailPage.cshtml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Admin.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace Admin.Pages.Users
+{
+
+    public class PaymentDetailPageModel : PageModel
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
+
+
+        public PaymentDetailPageModel(ApplicationDbContext context, UserManager<IdentityUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        [BindProperty]
+        public PaymentDetail PaymentDetail { get; set; }
+
+        public async Task OnGet()
+        {
+            var user = (await GetCurrentUserAsync()).Id;
+
+            PaymentDetail = await _context.PaymentDetails.FirstOrDefaultAsync(x => x.MemberId == user);
+        }
+
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            //if (!ModelState.IsValid)
+            //{
+            //    return Page();
+            //}
+
+            var userId = (await GetCurrentUserAsync()).Id;
+
+            if (!PaymentDetailExists(userId))
+            {
+                PaymentDetail.MemberId = userId;
+                _context.PaymentDetails.Add(PaymentDetail);
+                await _context.SaveChangesAsync();
+
+            }
+            else
+            {
+                var paymentDetail = await _context.PaymentDetails.FirstOrDefaultAsync(x => x.Id == PaymentDetail.Id);
+                paymentDetail.PinCode = PaymentDetail.PinCode;
+                paymentDetail.AmountPaid = PaymentDetail.AmountPaid;
+                paymentDetail.PaymentOption = PaymentDetail.PaymentOption;
+                paymentDetail.PaymentDate = PaymentDetail.PaymentDate;
+                await _context.SaveChangesAsync();
+            }
+
+
+            return Page();
+        }
+
+        private Task<IdentityUser> GetCurrentUserAsync() => _userManager.GetUserAsync(HttpContext.User);
+
+
+        private bool PaymentDetailExists(string id)
+        {
+            return _context.PaymentDetails.Any(e => e.MemberId == id);
+        }
+    }
+}
